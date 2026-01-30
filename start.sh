34b4aba30bbadf3397f229c70bd417b2d671dbe8
Ishak BELAHMAR <newishak@gmail.com>
2026-01-30 16:42:40 +0000

    Update start script

--- start.sh
+++ start.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 set -e
-
-if [ -n "${GIT_SSH_PRIVATE_KEY:-}" ]; then
-  mkdir -p /home/vibecoder/.ssh
-  printf "%s\n" "$GIT_SSH_PRIVATE_KEY" > /home/vibecoder/.ssh/id_rsa
-  chmod 600 /home/vibecoder/.ssh/id_rsa
-fi
-
-if [ -n "${GIT_COMMIT_USER_NAME:-}" ]; then
-  git config --global user.name "$GIT_COMMIT_USER_NAME"
-fi
-
-if [ -n "${GIT_COMMIT_USER_EMAIL:-}" ]; then
-  git config --global user.email "$GIT_COMMIT_USER_EMAIL"
-fi
-
 exec npm run start
